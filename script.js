4aec6d944bca985492476387cd0e9efed300a0c9
karthikch33 <saipavan39dh@gmail.com>
2025-03-03 12:41:14 +0530

    Lorem

---
+++ script.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const script = () => {
+  return (
+    <>
+    <div>script</div>
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum blanditiis illum atque, eos dolores quae architecto ea sed at a minus ducimus eligendi impedit, voluptas id commodi est repellat ipsa.
+    </>
+  )
+}
+
+export default script
